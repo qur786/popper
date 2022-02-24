c7e0d55129927a0c8fd2c378bd04fbfbdab1a0b8
Qurban <ahmad76qurban@gmail.com>
2022-02-24 21:54:33 +0530

    feat: add octo app create function

--- index.js
+++ index.js
@@ -3,20 +3,24 @@ import chalk from "chalk";
 import clear from "clear";
 import { Command } from "commander";
 import { isDirectoryExists, getCurrentDirectoryBase } from "./lib/files.js";
+import { setStoredGithubCredentials, createOctoApp } from "./lib/github-credentials.js";
 
 const CLI = new Command();
 
 CLI
     .command("start")
     .description("Start Popper")
-    .action(() => {
+    .argument("<token>", "Github Access Token")
+    .action(async (token) => {
         clear();
         console.log(chalk.magenta(figlet.textSync("Popper", {
             horizontalLayout: "default",
             verticalLayout: "default",
         })))
+        const res = await createOctoApp(token);
+        await setStoredGithubCredentials(token);
     });
-
+    
 CLI.parse(process.argv);
 
 if ( !CLI.args.length ) {
